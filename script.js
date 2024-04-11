f03e27b3d8d9c2f43598c06ce28761c876d34c79
BrandonMoran11 <bmoran2@ucol.mx>
2024-04-10 20:31:54 -0600

    corrección del SetTimeout

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let gif = document.getElementById('background');
+document.addEventListener("DOMContentLoaded", function () {
+  let gif = document.getElementById("background");
 
   // Función para cambiar el src de la imagen al segundo GIF después de un timeout
   function cambiarGIF() {
-    gif.style.backgroundImage  = 'url("assets/persona 3 gif2.gif")';
+    gif.style.backgroundImage = 'url("assets/persona 3 gif2.gif")';
   }
 
   // Cambiar el src después de 5 segundos (5000 milisegundos)
-  setTimeout(cambiarGIF, 5000);
+  setTimeout(cambiarGIF, 5800);
 });
